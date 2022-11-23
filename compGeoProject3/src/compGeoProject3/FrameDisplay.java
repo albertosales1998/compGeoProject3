380abc4fcf9c91c01d62f3a1ed01b052c91feaab
Beto <54753292+albertosales1998@users.noreply.github.com>
2022-11-23 17:30:22 -0500

    Initial Commit

---
+++ compGeoProject3/src/compGeoProject3/FrameDisplay.java
@@ -0,0 +1,28 @@
+package compGeoProject3;
+
+import javax.swing.JFrame;
+
+public class FrameDisplay extends JFrame{
+
+    private GraphDisplay panel;
+
+    /**
+     * Creates an initializes the window frame.
+     * 
+     * @param width frame width
+     * @param height frame height
+     * @param g geometric objects to be depicted in this frame
+     */
+    public FrameDisplay(int width, int height, GeometricObject[] g)
+    {
+        setTitle("Super Mario Bro 8-Bit Map"); 
+        setSize(width, height);
+        //setResizable(false);  
+ 
+        panel = new GraphDisplay(width, height, g);
+        add(panel);
+        pack(); //sizes the frame so that all its contents (panel in this case) 
+        //are at or above their preferred sizes
+    }
+	
+}
---
+++ compGeoProject3/src/compGeoProject3/GeometricObject.java
@@ -0,0 +1,61 @@
+package compGeoProject3;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public abstract class GeometricObject {
+
+    private Color boundaryColor;        //boundary color
+    private Color interiorColor;        //interior color
+
+    public GeometricObject()
+    {
+        interiorColor = Color.white;
+        boundaryColor = Color.black;
+    }
+
+    public GeometricObject(Color iC, Color bC) 
+    {
+        interiorColor = iC;
+        boundaryColor = bC;
+    }
+
+    abstract public void draw(Graphics g);
+
+    public Color getBoundaryColor()
+    {
+        return boundaryColor;
+    }
+    
+    public Color getInteriorColor()
+    {
+        return interiorColor;
+    }
+
+    public void setBoundaryColor(Color boundaryColor)
+    {
+        this.boundaryColor = boundaryColor;
+    }
+    
+    public void setInteriorColor(Color interiorColor)
+    {
+        this.interiorColor = interiorColor;
+    }
+
+    @Override
+    public String toString()
+    {
+        String str;
+        str = boundaryColor.getRed() + " " +
+              boundaryColor.getGreen() + " " +
+              boundaryColor.getBlue() + " " +
+              boundaryColor.getAlpha() + " " +
+              interiorColor.getRed() + " " +
+              interiorColor.getGreen() + " " +
+              interiorColor.getBlue() + " " +
+              interiorColor.getAlpha();
+        
+        return str;
+    }
+	
+}
