478791f68d185beea587fd37aa83f7b83f7dc2c6
Alberto Sales <albertosales@Albertos-MBP-2.hsd1.fl.comcast.net>
2022-11-27 16:29:18 -0500

    ten obstacles

--- compGeoProject3/src/compGeoProject3/Main.java
+++ compGeoProject3/src/compGeoProject3/Main.java
@@ -47,8 +47,7 @@ public class Main {
     	int frameHeight = 800;
         int dir = 0;//index of initial translation direction
         Random r = new Random();
-    	GeometricObject[] g = new GeometricObject[10];
-    	SimplePolygon[] sp = new SimplePolygon[10];
+    	SimplePolygon[] sp = new SimplePolygon[25];
     	Vector[] v = new Vector[8];//array of Vectors(Directions)
     	
     	v[0] = new Vector(0, -1);//north
@@ -61,24 +60,16 @@ public class Main {
     	v[7] = new Vector(-1,1);//
  
     	
-        Point p = new Point(5, 543);
-        Point p1 = new Point(5, 713);
-        Point p2 = new Point(249, 713);
-        Point p3 = new Point(249, 543);
-        Point p4 = new Point(200, 543);  
-        Point p5 = new Point(200, 429);
-        Point p6 = new Point(50, 429);
-        Point p7 = new Point(50, 543);
+        Point p = new Point(85, 200);
+        Point p1 = new Point(85, 217);
+        Point p2 = new Point(385, 217);
+        Point p3 = new Point(385, 200);
         sp[1] = new SimplePolygon();
         sp[1].addVertex(p);
         sp[1].addVertex(p1);
         sp[1].addVertex(p2);
         sp[1].addVertex(p3);
-        sp[1].addVertex(p4);
-        sp[1].addVertex(p5);
-        sp[1].addVertex(p6);
-        sp[1].addVertex(p7);  
-        //Tower Mario
+        //ULM L connector
 
         //top border
         SimplePolygon topBorder = new SimplePolygon(
@@ -118,80 +109,54 @@ public class Main {
                 });
         
         
-        p = new Point(500, 601);
-        p1 = new Point(500, 711);
-        p2 = new Point(695, 711);
-        p3 = new Point(695, 488); 
-        p4 = new Point(601, 488);
-        p5 = new Point(601, 604); 
-        p6 = new Point(500, 604); 
- 
+        p = new Point(565, 110);
+        p1 = new Point(565, 392);
+        p2 = new Point(585, 392);
+        p3 = new Point(585, 110);
         sp[2] = new SimplePolygon();
         sp[2].addVertex(p);
         sp[2].addVertex(p1);
         sp[2].addVertex(p2);
         sp[2].addVertex(p3);
-        sp[2].addVertex(p4);
-        sp[2].addVertex(p5);
-        sp[2].addVertex(p6);
-        //Mario pipe
+        //UMM body
         
-        p = new Point(346, 605);
-        p1 = new Point(346, 715);
-        p2 = new Point(394, 715);
-        p3 = new Point(394, 605); 
-          
+        p = new Point(85, 110);
+        p1 = new Point(85, 220);
+        p2 = new Point(100, 220);
+        p3 = new Point(100, 110);
         sp[3] = new SimplePolygon();
         sp[3].addVertex(p);
         sp[3].addVertex(p1);
         sp[3].addVertex(p2);
         sp[3].addVertex(p3);
-        //Mario
+        //ULM body
         
-        p = new Point(505, 372);
-        p1 = new Point(505, 395);
-        p2 = new Point(474, 395);
-        p3 = new Point(474, 454); 
-        p4 = new Point(575, 454);
-        p5 = new Point(575, 399); 
-        p6 = new Point(548, 399); 
-        p7 = new Point(548, 372); 
-        
-        
+        p = new Point(0, 110);
+        p1 = new Point(0, 130);
+        p2 = new Point(100, 130);
+        p3 = new Point(100, 110);
         sp[4] = new SimplePolygon();
         sp[4].addVertex(p);
         sp[4].addVertex(p1);
         sp[4].addVertex(p2);
         sp[4].addVertex(p3);
-        sp[4].addVertex(p4);
-        sp[4].addVertex(p5);
-        sp[4].addVertex(p6);
-        sp[4].addVertex(p7);
-        //Cloud 1
+        //uppermost left-obstacle
         
-        p = new Point(204, 142);
-        p1 = new Point(204, 172);
-        p2 = new Point(176, 172);
-        p3 = new Point(176, 229);  
-        p4 = new Point(325, 229);
-        p5 = new Point(325, 172);
-        p6 = new Point(300, 172);
-        p7 = new Point(300, 142); 
+        p = new Point(150, 110);
+        p1 = new Point(150, 130);
+        p2 = new Point(720, 130);
+        p3 = new Point(720, 110);
         sp[5] = new SimplePolygon();
         sp[5].addVertex(p);
         sp[5].addVertex(p1);
         sp[5].addVertex(p2); 
         sp[5].addVertex(p3);
-        sp[5].addVertex(p4);
-        sp[5].addVertex(p5);
-        sp[5].addVertex(p6); 
-        sp[5].addVertex(p7);
-        //Cloud 2
+        //Uppermost mid-obstacle
         
-        p = new Point(982/2, 962/2);
-        p1 = new Point(982/2, (962/2)+10);
-        p2 = new Point((982/2)+10, (962/2)+10);
-        p3 = new Point((982/2)+10, 962/2); 
+        p = new Point(978/2, 962/2);
+        p1 = new Point(978/2, (962/2)+10);
+        p2 = new Point((978/2)+10, (962/2)+10);
+        p3 = new Point((978/2)+10, 962/2);
         sp[0] = new SimplePolygon();
         sp[0].addVertex(p);
         sp[0].addVertex(p1);
@@ -205,8 +170,8 @@ public class Main {
 	    frame.setVisible(true);
 	    
         p = new Point(0,0);
-        p1 = new Point(0,84);
-        p2 = new Point(800,84);
+        p1 = new Point(0,64);
+        p2 = new Point(800,64);
         p3 = new Point(800,0); 
         sp[6] = new SimplePolygon();
         sp[6].addVertex(p);
@@ -217,8 +182,8 @@ public class Main {
         
         p = new Point(0,0);
         p1 = new Point(0,800);
-        p2 = new Point(5,800);
-        p3 = new Point(5,0); 
+        p2 = new Point(30,800);
+        p3 = new Point(30,0);
         sp[7] = new SimplePolygon();
         sp[7].addVertex(p);
         sp[7].addVertex(p1);
@@ -226,10 +191,10 @@ public class Main {
         sp[7].addVertex(p3);
         //wall left
         
-        p = new Point(0,712);
+        p = new Point(0,750);
         p1 = new Point(0,800);
         p2 = new Point(800,800);
-        p3 = new Point(800,712); 
+        p3 = new Point(800,750);
         sp[8] = new SimplePolygon();
         sp[8].addVertex(p);
         sp[8].addVertex(p1);
@@ -237,8 +202,8 @@ public class Main {
         sp[8].addVertex(p3);
         //wall down
         
-        p = new Point(795,0);
-        p1 = new Point(795,800);
+        p = new Point(770,0);
+        p1 = new Point(770,800);
         p2 = new Point(800,800);
         p3 = new Point(800,0); 
         sp[9] = new SimplePolygon();
@@ -247,6 +212,171 @@ public class Main {
         sp[9].addVertex(p2); 
         sp[9].addVertex(p3); 
         //wall right
+
+        p = new Point(700,200);
+        p1 = new Point(700,306);
+        p2 = new Point(720,306);
+        p3 = new Point(720,200);
+        sp[10] = new SimplePolygon();
+        sp[10].addVertex(p);
+        sp[10].addVertex(p1);
+        sp[10].addVertex(p2);
+        sp[10].addVertex(p3);
+        //rotated T vertical bar
+
+        p = new Point(635,240);
+        p1 = new Point(635,264);
+        p2 = new Point(720,264);
+        p3 = new Point(720,240);
+        sp[11] = new SimplePolygon();
+        sp[11].addVertex(p);
+        sp[11].addVertex(p1);
+        sp[11].addVertex(p2);
+        sp[11].addVertex(p3);
+        //rotated T hori bar
+
+        p = new Point(135,200);
+        p1 = new Point(135,305);
+        p2 = new Point(150,305);
+        p3 = new Point(150,200);
+        sp[12] = new SimplePolygon();
+        sp[12].addVertex(p);
+        sp[12].addVertex(p1);
+        sp[12].addVertex(p2);
+        sp[12].addVertex(p3);
+        //ULM T vert connector
+
+        p = new Point(85,285);
+        p1 = new Point(85,305);
+        p2 = new Point(220,305);
+        p3 = new Point(220,285);
+        sp[13] = new SimplePolygon();
+        sp[13].addVertex(p);
+        sp[13].addVertex(p1);
+        sp[13].addVertex(p2);
+        sp[13].addVertex(p3);
+        //ULM T hori connector
+
+        p = new Point(265,200);
+        p1 = new Point(265,305);
+        p2 = new Point(285,305);
+        p3 = new Point(285,200);
+        sp[14] = new SimplePolygon();
+        sp[14].addVertex(p);
+        sp[14].addVertex(p1);
+        sp[14].addVertex(p2);
+        sp[14].addVertex(p3);
+        //ULM Vert bar
+
+        p = new Point(567,370);
+        p1 = new Point(567,393);
+        p2 = new Point(717,393);
+        p3 = new Point(717,370);
+        sp[15] = new SimplePolygon();
+        sp[15].addVertex(p);
+        sp[15].addVertex(p1);
+        sp[15].addVertex(p2);
+        sp[15].addVertex(p3);
+        //odd T
+
+        p = new Point(500,200);
+        p1 = new Point(500,695);
+        p2 = new Point(517,695);
+        p3 = new Point(517,200);
+        sp[16] = new SimplePolygon();
+        sp[16].addVertex(p);
+        sp[16].addVertex(p1);
+        sp[16].addVertex(p2);
+        sp[16].addVertex(p3);
+        //odd T
+
+        p = new Point(500,565);
+        p1 = new Point(500,590);
+        p2 = new Point(650,590);
+        p3 = new Point(650,565);
+        sp[17] = new SimplePolygon();
+        sp[17].addVertex(p);
+        sp[17].addVertex(p1);
+        sp[17].addVertex(p2);
+        sp[17].addVertex(p3);
+        //odd T
+
+        p = new Point(435,200);
+        p1 = new Point(435,610);
+        p2 = new Point(450,610);
+        p3 = new Point(450,200);
+        sp[18] = new SimplePolygon();
+        sp[18].addVertex(p);
+        sp[18].addVertex(p1);
+        sp[18].addVertex(p2);
+        sp[18].addVertex(p3);
+        //h shape
+
+        p = new Point(267,500);
+        p1 = new Point(267,525);
+        p2 = new Point(450,525);
+        p3 = new Point(450,500);
+        sp[19] = new SimplePolygon();
+        sp[19].addVertex(p);
+        sp[19].addVertex(p1);
+        sp[19].addVertex(p2);
+        sp[19].addVertex(p3);
+        //h shape
+
+        p = new Point(268,500);
+        p1 = new Point(268,610);
+        p2 = new Point(285,610);
+        p3 = new Point(286,500);
+        sp[20] = new SimplePolygon();
+        sp[20].addVertex(p);
+        sp[20].addVertex(p1);
+        sp[20].addVertex(p2);
+        sp[20].addVertex(p3);
+        //h shape
+
+        p = new Point(367,285);
+        p1 = new Point(367,392);
+        p2 = new Point(385,392);
+        p3 = new Point(385,285);
+        sp[21] = new SimplePolygon();
+        sp[21].addVertex(p);
+        sp[21].addVertex(p1);
+        sp[21].addVertex(p2);
+        sp[21].addVertex(p3);
+        //zig zag
+
+        p = new Point(200,370);
+        p1 = new Point(200,392);
+        p2 = new Point(385,392);
+        p3 = new Point(385,370);
+        sp[22] = new SimplePolygon();
+        sp[22].addVertex(p);
+        sp[22].addVertex(p1);
+        sp[22].addVertex(p2);
+        sp[22].addVertex(p3);
+        //zig zag
+
+        p = new Point(200,370);
+        p1 = new Point(200,610);
+        p2 = new Point(218,610);
+        p3 = new Point(218,370);
+        sp[23] = new SimplePolygon();
+        sp[23].addVertex(p);
+        sp[23].addVertex(p1);
+        sp[23].addVertex(p2);
+        sp[23].addVertex(p3);
+        //zig zag
+
+        p = new Point(0,370);
+        p1 = new Point(0,393);
+        p2 = new Point(150,393);
+        p3 = new Point(150, 370);
+        sp[24] = new SimplePolygon();
+        sp[24].addVertex(p);
+        sp[24].addVertex(p1);
+        sp[24].addVertex(p2);
+        sp[24].addVertex(p3);
+        //left wall sticking out big
      
     	while(true)
     	{
